923164e804ae3f4ef4e74b7a9eda91741af419b9
Obit01337 <74430714+Obit01337@users.noreply.github.com>
2020-11-25 20:47:15 +0300

    yeah

--- main.c
+++ main.c
@@ -14,14 +14,14 @@
 int check_correct_input(void);
 void text_input(char** text, int num_str, int size_str);
 void text_output(char** text, int num_str);
-int find_words_of_letters(char** text, int num_str, int size_str);
-int find_words_of_numbers(char** text, int num_str, int size_str);
-int find_words_of_sings(char** text, int num_str, int size_str);
+int find_words_of_letters(char** text, int num_str);
+int find_words_of_numbers(char** text, int num_str);
+int find_words_of_sings(char** text, int num_str);
 
 int main(void)
 {
     // Input text size
-    puts("Enter number of strings: ");
+    puts("Enter number of strings:");
     int num_str = check_correct_input();
     puts("Enter max length of string");
     int size_str = check_correct_input();
@@ -49,11 +49,11 @@ int main(void)
     // Output text
     text_output(text, num_str);
     // Finding number of words and print it in console
-    int count_let_words = find_words_of_letters(text, num_str, size_str);
+    int count_let_words = find_words_of_letters(text, num_str);
     printf_s("num words of letters = %d\n", count_let_words);
-    int count_num_words = find_words_of_numbers(text, num_str, size_str);
+    int count_num_words = find_words_of_numbers(text, num_str);
     printf_s("num words of numbers = %d\n", count_num_words);
-    int count_sing_words = find_words_of_sings(text, num_str, size_str);
+    int count_sing_words = find_words_of_sings(text, num_str);
     printf_s("num words of sings = %d\n", count_sing_words);
     // Finding the biggest number of words
     if (count_let_words > count_num_words && count_let_words > count_sing_words)
@@ -122,11 +122,11 @@ void text_input(char** text, int num_str, int size_str)
 }
 
 // 3 word count functions
-int find_words_of_letters(char** text, int num_str, int size_str)
+int find_words_of_letters(char** text, int num_str)
 {
     int num_words = 0;
     for (int i = 0; i < num_str; i++)
-        for (int j = 0; j < size_str; j++)
+        for (int j = 0; text[i][j] != '\0'; j++)
         {
             int first = 1;
             for (int k = j; text[i][k] >= 97 && text[i][k] <= 122; k++)
@@ -142,11 +142,11 @@ int find_words_of_letters(char** text, int num_str, int size_str)
     return num_words;
 }
 
-int find_words_of_numbers(char** text, int num_str, int size_str)
+int find_words_of_numbers(char** text, int num_str)
 {
     int num_words = 0;
     for (int i = 0; i < num_str; i++)
-        for (int j = 0; j < size_str; j++)
+        for (int j = 0; j < text[i][j] != '\0'; j++)
         {
             int first = 1;
             for (int k = j; text[i][k] >= 48 && text[i][k] <= 57; k++)
@@ -162,11 +162,11 @@ int find_words_of_numbers(char** text, int num_str, int size_str)
     return num_words;
 }
 
-int find_words_of_sings(char** text, int num_str, int size_str)
+int find_words_of_sings(char** text, int num_str)
 {
     int num_words = 0;
     for (int i = 0; i < num_str; i++)
-        for (int j = 0; j < size_str; j++)
+        for (int j = 0; text[i][j] != '\0'; j++)
         {
             int first = 1;
             for (int k = j; text[i][j] == 42 || text[i][j] == 43 || text[i][j] == 45; k++)
